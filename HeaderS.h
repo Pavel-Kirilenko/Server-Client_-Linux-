cb02a8541903cc620a55c9efcbb15931ebab771f
Pavel-Kirilenko <pavelkirilenko@mail.ru>
2024-11-04 23:23:56 +0400

    Убрать строку using namespace std; из заголовочного файла

--- HeaderS.h
+++ HeaderS.h
@@ -3,17 +3,15 @@
 #include <map>
 #include <vector>
 
-using namespace std;
-
-void userReg(string& curName, string& curPassWord);  // enter user's data while registaring or loggin in
+void userReg(std::string& curName, std::string& curPassWord);  // enter user's data while registaring or loggin in
 
 class Chat
 {
-    map <string, string> persArray;               // array of user's data
+    std::map <std::string, std::string> persArray;             // array of user's data
 public:
-    void entrChat(bool& persPresence, string& curName, string& curPassWord);     // to log in chat
-    void regChat(bool& nameInChatAlready, bool& persPresence, string& curName, string& curPassWord);    // to register in chat
-    void sendMess(string curName);                // to send a message
+    void entrChat(bool& persPresence, std::string& curName, std::string& curPassWord);     // to log in chat
+    void regChat(bool& nameInChatAlready, bool& persPresence, std::string& curName, std::string& curPassWord);    // to register in chat
+    void sendMess(std::string curName);                        // to send a message
 };
 
 // 24.09.2024 by Pavel Kirilenko
